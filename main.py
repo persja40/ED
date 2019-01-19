764f04e07da6b2c1aaa9289627dd60ae6c42363b
persja40 <persja40@gmail.com>
2019-01-19 20:19:12 +0100

    AgglomerativeClustering

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+from sklearn.metrics import pairwise_distances
+from sklearn.cluster import AgglomerativeClustering
 from statistics import mean
 from sklearn.cluster import KMeans
 from sklearn.neighbors import NearestNeighbors
@@ -11,96 +13,135 @@ from sklearn.metrics import confusion_matrix
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-
-data = pd.read_csv('StudentsPerformance.csv')
-samples = data.values
-
-# import ed_stat
-# ed_stat.race_percentage(samples)
-
-n_neighbors = 5
-
-
-# OUTLIERS
-
-
-edu_dict = {"bachelor's degree": 0.8, 'some college': 0.6, "master's degree": 1,
-            "associate's degree": 0.8, 'high school': 0.5, 'some high school': 0}
-
-encoder_dict = {
-    'gender': LabelEncoder().fit(samples[:, 0]), 'race': LabelEncoder().fit(samples[:, 1]),
-    'parents_edu': LabelEncoder().fit(samples[:, 2]), 'lunch': LabelEncoder().fit(samples[:, 3]),
-    'prep_test': LabelEncoder().fit(samples[:, 4])
-}
-
-exams_list = ["math score", "reading score", "writing score"]
-
+import multiprocessing as mp
 
 def gower_distance(e1, e2):
-    result = 0
-    # categorical
-    for i in [0, 1, 3, 4]:
-        if e1[i] != e2[i]:
-            result += 1
+        result = 0
+        # categorical
+        for i in [0, 1, 3, 4]:
+            if e1[i] != e2[i]:
+                result += 1
 
-    # exam scores
-    for i in [5, 6, 7]:
-        result += abs(edu_dict[encoder_dict['parents_edu'].inverse_transform([int(e1[2])])[0]] -
-                      edu_dict[encoder_dict['parents_edu'].inverse_transform([int(e2[2])])[0]])
-    return result
+        # exam scores
+        for i in [5, 6, 7]:
+            result += abs(edu_dict[encoder_dict['parents_edu'].inverse_transform([int(e1[2])])[0]] -
+                        edu_dict[encoder_dict['parents_edu'].inverse_transform([int(e2[2])])[0]])
+        return result
 
+def gower_affinity(X):
+        return pairwise_distances(X, metric=gower_distance)
 
-samples[:, 0] = encoder_dict['gender'].transform(samples[:, 0])
-samples[:, 1] = encoder_dict['race'].transform(samples[:, 1])
-samples[:, 2] = encoder_dict['parents_edu'].transform(samples[:, 2])
-samples[:, 3] = encoder_dict['lunch'].transform(samples[:, 3])
-samples[:, 4] = encoder_dict['prep_test'].transform(samples[:, 4])
+def make_cluster(params):
+    (cluster_nr, filtered_samples, encoder_dict) = params
+    # print(cluster_nr)
+    # print(filtered_samples)
+    # print(encoder_dict)
+    cluster = AgglomerativeClustering(
+        n_clusters=cluster_nr, affinity=gower_affinity, linkage="average")
 
-print(encoder_dict['parents_edu'].inverse_transform([1]))
+    data_clusters = [[] for i in range(cluster_nr)]
+    cluster_label = cluster.fit_predict(filtered_samples)
+    for i in range(len(filtered_samples)):
+        data_clusters[cluster_label[i]].append(filtered_samples[i])
 
-nbrs = NearestNeighbors(n_neighbors=n_neighbors, algorithm='brute',
-                        metric=gower_distance).fit(samples)
-
-# 1% of samples take as outliers
-p = len(samples) * 0.01
-
-sample_dict = {}
-samples_range = [i for i in range(len(samples))]
-outliers = []
-old_value = -1
-
-for i, s in enumerate(samples):
-    distances, indices = nbrs.kneighbors([s])
-    sample_dict[i] = mean(distances[0])
-
-for key in sorted(sample_dict, key=sample_dict.get, reverse=True):
-    value = sample_dict[key]
-    if p <= 0 and old_value != value:
-        break
-    old_value = value
-    p -= 1
-    outliers.append([key, value])
-    samples_range.remove(key)
-
-filtered_samples = np.array([samples[i] for i in samples_range])
-
-with open('stats/outliers.dat', 'w') as f:
-    for o in outliers:
-        i = o[0]
-        decode = [
-            encoder_dict['gender'].inverse_transform([samples[i][0]])[0],
-            encoder_dict['race'].inverse_transform([samples[i][1]])[0],
-            encoder_dict['parents_edu'].inverse_transform([samples[i][2]])[0],
-            encoder_dict['lunch'].inverse_transform([samples[i][3]])[0],
-            encoder_dict['prep_test'].inverse_transform([samples[i][4]])[0]
-        ]
-        decode.extend(samples[i][5:8])
-
-        f.write(str(i) + ' \tdistance={:.2f}'.format(o[1]) + ', ' + str(decode) + '\n')
+    # # print(data_clusters)
+    # for e in data_clusters:
+    #     print(len(e))
+    for i in range(cluster_nr):
+        with open("stats_cl_"+ str(cluster_nr) +"/cluster_"+str(i)+".dat", "w") as f:
+            for e in data_clusters[i]:
+                f.write(encoder_dict['gender'].inverse_transform([e[0]])[0] + " , " + encoder_dict['race'].inverse_transform([e[1]])[0] + " , " + encoder_dict['parents_edu'].inverse_transform([e[2]])[0] + " , " + encoder_dict['lunch'].inverse_transform([e[3]])[0] +
+                        " , " + encoder_dict['prep_test'].inverse_transform([e[4]])[0] + " , " + str(e[5]) + " , " + str(e[6]) + " , " + str(e[7])+"\n")
 
 
+if __name__ == "__main__":
+    data = pd.read_csv('StudentsPerformance.csv')
+    samples = data.values
+
+    # import ed_stat
+    # ed_stat.race_percentage(samples)
+
+    n_neighbors = 5
+
+
+    # OUTLIERS
+
+
+    edu_dict = {"bachelor's degree": 0.8, 'some college': 0.6, "master's degree": 1,
+                "associate's degree": 0.8, 'high school': 0.5, 'some high school': 0}
+
+    encoder_dict = {
+        'gender': LabelEncoder().fit(samples[:, 0]), 'race': LabelEncoder().fit(samples[:, 1]),
+        'parents_edu': LabelEncoder().fit(samples[:, 2]), 'lunch': LabelEncoder().fit(samples[:, 3]),
+        'prep_test': LabelEncoder().fit(samples[:, 4])
+    }
+
+    exams_list = ["math score", "reading score", "writing score"]
+
+    samples[:, 0] = encoder_dict['gender'].transform(samples[:, 0])
+    samples[:, 1] = encoder_dict['race'].transform(samples[:, 1])
+    samples[:, 2] = encoder_dict['parents_edu'].transform(samples[:, 2])
+    samples[:, 3] = encoder_dict['lunch'].transform(samples[:, 3])
+    samples[:, 4] = encoder_dict['prep_test'].transform(samples[:, 4])
+
+    nbrs = NearestNeighbors(n_neighbors=n_neighbors, algorithm='brute',
+                            metric=gower_distance).fit(samples)
+
+    # 5% of samples take as outliers
+    p = len(samples) * 0.05
+
+    sample_dict = {}
+    samples_range = [i for i in range(len(samples))]
+    outliers = []
+    old_value = -1
+
+    for i, s in enumerate(samples):
+        distances, indices = nbrs.kneighbors([s])
+        sample_dict[i] = mean(distances[0])
+
+    for key in sorted(sample_dict, key=sample_dict.get, reverse=True):
+        value = sample_dict[key]
+        if p <= 0 and old_value != value:
+            break
+        old_value = value
+        p -= 1
+        outliers.append([key, value])
+        samples_range.remove(key)
+
+    filtered_samples = np.array([samples[i] for i in samples_range])
+
+    with open('stats/outliers.dat', 'w') as f:
+        for o in outliers:
+            i = o[0]
+            decode = [
+                encoder_dict['gender'].inverse_transform([samples[i][0]])[0],
+                encoder_dict['race'].inverse_transform([samples[i][1]])[0],
+                encoder_dict['parents_edu'].inverse_transform([samples[i][2]])[0],
+                encoder_dict['lunch'].inverse_transform([samples[i][3]])[0],
+                encoder_dict['prep_test'].inverse_transform([samples[i][4]])[0]
+            ]
+            decode.extend(samples[i][5:8])
+
+            f.write( str(i) + ' \tdistance={:.2f}'.format(o[1]) + ', ' + str(decode) + '\n')
+
+
+
+
+
+
+    # CLUSTER
+    params = []
+    for cl_n in range(3,11):
+        params.append((cl_n, filtered_samples, encoder_dict))
+    with mp.Pool(8) as p:
+        p.map(make_cluster, params)
+    
+
+"""
 # CLUSTER
-
+for e in filtered_samples:
+    e[1] = 0
+print("SUCCESS")
 
 cluster_nr = 10
 classes = len(filtered_samples) * [0]
@@ -291,3 +332,5 @@ with open('stats/class_stats.dat', 'w') as f:
                                                                    class_stats[i][j + length][2],
                                                                    class_stats[i][j + length][3]) + ', ')
         f.write('\n')
+
+"""
